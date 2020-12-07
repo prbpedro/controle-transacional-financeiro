354a86e414d29f03bae1b98202ba7caf0c3fd484
Pedro Ribeiro Baptista <prbpedro@gmail.com>
2020-12-07 17:41:39 -0300

    Remove references to implementations

--- src/ctf/README.md
+++ src/ctf/README.md
@@ -1,3 +1,4 @@
 mvn clean compile test jacoco:report 
 
 http://localhost:8080/swagger-ui.html
+http://localhost:8080/database
--- src/ctf/src/main/java/com/github/prbpedro/ctf/Application.java
+++ src/ctf/src/main/java/com/github/prbpedro/ctf/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.github.prbpedro.ctf.services.impl.OperationTypeService;
+import com.github.prbpedro.ctf.services.IOperationTypeService;
 
 @SpringBootApplication
 public class Application {
@@ -12,6 +12,6 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
-		context.getBean(OperationTypeService.class).persistDefaultValues();
+		context.getBean(IOperationTypeService.class).persistDefaultValues();
 	}
 }
